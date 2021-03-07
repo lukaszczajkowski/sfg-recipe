6577ff51ff0d051f88af46e5ff1bedbe3d67c1f9
Lukasz Czajkowski <lukaszczajkowski.wroc@gmail.com>
2021-03-07 20:10:01 +0100

    Initial commit

---
+++ src/test/java/sfg/guru/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package sfg.guru.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
